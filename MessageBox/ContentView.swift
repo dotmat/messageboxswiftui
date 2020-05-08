f085b1a2e4c4d2e3ea330943382adc292f1d5ca7
Mathew Jenkinson <mathew@mbmlabs.com>
2020-05-08 09:49:06 -0400

    Initial Commit

---
+++ MessageBox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessageBox
+//
+//  Created by Mathew Jenkinson on 5/8/20.
+//  Copyright © 2020 Mathew Jenkinson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
